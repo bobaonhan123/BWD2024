9b64edbc7ec3e07da46de97d7a819a0789766306
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-06-26 04:11:10 +0700

    update scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 function Footer() {
     return (
         <footer className='w-full mx-0 my-0
-    h-64 mt-10'>
+        h-64 mt-10'>
             <hr className="border-t-1 border-[#6c7580a8] mx-64" />
             <div className='flex
-    items-center
-    justify-between
-    h-52
-    px-10
-    mx-52
-    '>
+            items-center
+            justify-between
+            h-52
+            px-10
+            mx-52
+            '>
                 <div className="flex flex-col items-start">
                     <p className='font-semibold text-lg'>About</p>
                     <p className='text-sm text-[#6C7580]'>Pricing</p>
