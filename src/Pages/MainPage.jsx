a39afc0e8ab9e1e52344009e89986aef3b4c8a8d
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-06-26 02:27:17 +0700

    mainpage

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,18 +1,44 @@
-
-
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 function MainPage() {
     return (
-        <div className='mt-11'>
-            <div>
-                <h1 className='font-semibold text-7xl mt-28'>
+        <div className='mt-11 w-full'>
+            <div className='w-full flex flex-col items-center'>
+                <h1 className='font-semibold text-7xl mt-28 mb-10'>
                     <p className='bg-gradient-to-r
                     from-[#00F2FE] from-21%
-                to-[#4FACFE] bg-clip-text text-transparent'>
+                to-[#4FACFE] bg-clip-text text-transparent
+                h-28'>
                     Boost your vocabulary</p>
-                    <p className='text-[#4B5563]'>
+                    <p className='text-[#4B5563] h-24'>
                     and language skills with us</p>
                 </h1>
-            
+                <div className='
+                flex items-center 
+                justify-center
+                w-full'>
+                    <p className='text-[#6C7580] text-lg mx-4'>
+                        <FontAwesomeIcon icon={faCircleCheck} className='text-[#4FACFE] w-6' />
+                        Memorize with Active Learning Methods
+                    </p>
+                    <p className='text-[#6C7580] text-lg mx-4'>
+                        <FontAwesomeIcon icon={faCircleCheck} className='text-[#4FACFE] w-6' />
+                        Impove Speaking Skill with AI
+                    </p>
+                    <p className='text-[#6C7580] text-lg mx-4'>
+                        <FontAwesomeIcon icon={faCircleCheck} className='text-[#4FACFE] w-6' />
+                        Gamify learning with word games
+                    </p>
+                </div>
+                <a href='#' className='ml-8 text-base h-12 
+                    bg-gradient-to-r from-[#00F2FE] from-21%
+                    to-[#4FACFE]
+                    px-10 my-10
+                    flex items-center rounded-full justify-center' >
+                        <p className='text-white font-semibold text-lg' >
+                            Let’s start explore the magic
+                        </p>
+                    </a>
             </div>
         </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ function Footer() {
     return (
         <footer className='w-full mx-0 my-0
     h-64 mt-10'>
-            <hr className="border-t-1 border-[#6c7580a8] mx-52" />
+            <hr className="border-t-1 border-[#6c7580a8] mx-64" />
             <div className='flex
     items-center
     justify-between
     h-52
-    px-4
+    px-10
     mx-52
     '>
                 <div className="flex flex-col items-start">
@@ -16,23 +16,23 @@ function Footer() {
                     <p className='text-sm text-[#6C7580]'>Contact</p>
                 </div>
                 <div className="flex flex-col items-start">
-                    <p className='font-semibold text-lg'>About</p>
-                    <p className='text-sm text-[#6C7580]'>Pricing</p>
-                    <p className='text-sm text-[#6C7580]'>Contact</p>
+                    <p className='font-semibold text-lg'>Product</p>
+                    <p className='text-sm text-[#6C7580]'>Update</p>
+                    <p className='text-sm text-[#6C7580]'>Blog</p>
                 </div>
                 <div className="flex flex-col items-start">
-                    <p className='font-semibold text-lg'>About</p>
-                    <p className='text-sm text-[#6C7580]'>Pricing</p>
-                    <p className='text-sm text-[#6C7580]'>Contact</p>
+                    <p className='font-semibold text-lg'>Social</p>
+                    <p className='text-sm text-[#6C7580]'>Facebook</p>
+                    <p className='text-sm text-[#6C7580]'>Instagram</p>
                 </div>
                 <div className="flex flex-col items-start">
-                    <p className='font-semibold text-lg'>About</p>
-                    <p className='text-sm text-[#6C7580]'>Pricing</p>
-                    <p className='text-sm text-[#6C7580]'>Contact</p>
+                    <p className='font-semibold text-lg'>Help & Info</p>
+                    <p className='text-sm text-[#6C7580]'>Terms And Conditions</p>
+                    <p className='text-sm text-[#6C7580]'>Privacy Policy</p>
                 </div>
             </div>
             <p className='text-sm mb-2 text-[#4B5563]'>Made by Khỉ Con</p>
-            <p className='text-sm text-[#6C7580]'>© TakeItEasy 2024. All Rights Reserved.</p>
+            <p className='text-sm text-[#6C7580] pb-8'>© TakeItEasy 2024. All Rights Reserved.</p>
         </footer>
     );
 }
