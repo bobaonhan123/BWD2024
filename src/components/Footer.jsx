d4a2a7348335d752ac7191f4e74a70afeec18121
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-06-26 05:09:12 +0700

    update backgournd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ function Footer() {
     return (
         <footer className='w-full mx-0 my-0
         h-64 mt-10'>
-            <hr className="border-t-1 border-[#6c7580a8] mx-64" />
+            <hr className="border-t-1 border-[#6c7580a8] mx-96" />
             <div className='flex
             items-center
             justify-between
             h-52
             px-10
-            mx-52
+            mx-80
             '>
                 <div className="flex flex-col items-start">
                     <p className='font-semibold text-lg'>About</p>
