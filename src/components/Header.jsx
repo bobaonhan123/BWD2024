fb3bf6fd769c1e7d172b718236293aabe56299ca
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-06-26 01:02:47 +0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,21 +17,31 @@ function Header() {
         mx-auto
         max-w-7xl' >
                 <div className='flex items-center' >
-                    <h1 className='ml-2 text-xl font-semibold' >
+                    <h1 className='ml-2 text-xl font-semibold
+                    bg-gradient-to-b from-[#4FACFE] from-21%
+                    to-[#00F2FE] bg-clip-text text-transparent' >
                         take it easy
                     </h1>
                 </div>
-                <nav className='flex items-center' >
-                    <a href='#' className='ml-8 text-lg' >
+                <nav className='flex items-center ' >
+                    <a href='#' className='ml-8 text-base text-[#6C7580]' >
                         Pricing
                     </a>
-                    <a href='#' className='ml-8 text-lg' >
+                    <a href='#' className='ml-8 text-base text-[#6C7580]' >
                         About us
                     </a>
-                    <a href='#' className='ml-8 text-lg' >
-                       Login
+                    <a href='#' className='ml-8 text-base text-[#6C7580]' >
+                        Login
                     </a>
-                    
+                    <a href='#' className='ml-8 text-base h-10 
+                    bg-gradient-to-r from-[#00F2FE] from-21%
+                    to-[#4FACFE]
+                    px-6
+                    flex items-center rounded-full' >
+                        <p className='text-white' >
+                            Get Started
+                        </p>
+                    </a>
                 </nav>
             </div>
 
