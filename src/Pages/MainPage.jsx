31115dfc36dc53d9ea26dc9ecd5779cdf259354b
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-06-29 23:42:14 +0700

    responsive header,footer,mainpage

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -8,15 +8,22 @@ function MainPage() {
                     <p className='bg-gradient-to-r
                     from-[#00F2FE] from-21%
                 to-[#4FACFE] bg-clip-text text-transparent
-                h-28'>
+                h-28
+                max-lg:h-auto
+                max-lg:text-5xl
+                '>
                     Boost your vocabulary</p>
-                    <p className='text-[#4B5563] h-24'>
+                    <p className='text-[#4B5563] h-24
+                    max-lg:h-auto
+                    max-lg:text-5xl'>
                     and language skills with us</p>
                 </h1>
                 <div className='
                 flex items-center 
                 justify-center
-                w-full'>
+                w-full
+                max-lg:flex-col
+                max-lg:items-start'>
                     <p className='text-[#6C7580] text-lg mx-4'>
                         <FontAwesomeIcon icon={faCircleCheck} className='text-[#4FACFE] w-6' />
                         Memorize with Active Learning Methods
@@ -30,12 +37,15 @@ function MainPage() {
                         Gamify learning with word games
                     </p>
                 </div>
-                <a href='#' className='ml-8 text-base h-12 
+                <a href='#' className='mx-8 text-base h-12 
                     bg-gradient-to-r from-[#00F2FE] from-21%
                     to-[#4FACFE]
                     px-10 my-10
-                    flex items-center rounded-full justify-center' >
-                        <p className='text-white font-semibold text-lg' >
+                    flex items-center rounded-full justify-center
+                    max-md:px-5
+                    max-md:mx-5' >
+                        <p className='text-white font-semibold text-lg
+                        max-md:text-base' >
                             Let’s start explore the magic
                         </p>
                 </a>
