b64f31f968b7c6b3a98d54050430e34f76fd8491
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-09-04 22:48:05 +0700

    remoce top learners

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getUser } from "../api/userAPI";
+import { getUser } from "../api/userAPI"; // Assume logoutUser is a function to handle logout
 import { useNavigate } from "react-router-dom";
 
 function RightBar() {
@@ -10,6 +10,7 @@ function RightBar() {
     vocabularies: [],
     level: 0,
   });
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -25,92 +26,48 @@ function RightBar() {
       }
     }
     fetchData();
-  }, []);
+  }, [navigate]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
   return (
-    <div
-      className={`fixed max-w-96 w-1/5 h-screen right-0 top-0 flex flex-col items-center`}
-    >
-      <div
-        className="flex flex-col items-center mt-28 bg-white w-5/6 rounded-3xl
-      relative
-      pt-10
-      shadow-md"
-      >
+    <div className="fixed max-w-96 w-1/5 h-screen right-0 top-0 flex flex-col items-center">
+      <div className="flex flex-col items-center mt-28 bg-white w-5/6 rounded-3xl relative pt-10 shadow-md">
         <img
           src="https://variety.com/wp-content/uploads/2023/12/MCDAVTH_WD032.jpg"
           alt="avt"
-          className="w-24 h-24
-        absolute rounded-full
-        top-[-4.5rem] z-50
-        object-cover
-        border-4 border-white
-        "
+          className="w-24 h-24 absolute rounded-full top-[-4.5rem] z-50 object-cover border-4 border-white"
         />
-
-        <h1
-          className="
-        text-[#4b5563]"
-        >
-          {info.fullname}
-        </h1>
-        <p
-          className="text-sm
-        text-[#6C7580]"
-        >
-          {info.email}
-        </p>
-        <div
-          className="flex justify-between w-full
-        my-4"
-        >
-          <div
-            className="flex flex-col items-center w-1/2
-          border
-          border-y-0
-            border-l-0
-          border-r-[#73737355]"
-          >
-            <p
-              className="
-            text-[#6C7580]"
-            >
-              Vocabs
-            </p>
-            <h1
-              className="text-[#4b5563]
-            font-semibold
-            text-3xl"
-            >
+        <h1 className="text-[#4b5563]">{info.fullname}</h1>
+        <p className="text-sm text-[#6C7580]">{info.email}</p>
+        <div className="flex justify-between w-full my-4">
+          <div className="flex flex-col items-center w-1/2 border border-y-0 border-l-0 border-r-[#73737355]">
+            <p className="text-[#6C7580]">Vocabs</p>
+            <h1 className="text-[#4b5563] font-semibold text-3xl">
               {info.vocabularies.length}
             </h1>
           </div>
           <div className="flex flex-col items-center w-1/2">
-            <p
-              className="
-            text-[#6C7580]"
-            >
-              Level
-            </p>
-            <h1
-              className="text-[#4b5563]
-            font-semibold
-            text-3xl"
-            >
-              1
+            <p className="text-[#6C7580]">Level</p>
+            <h1 className="text-[#4b5563] font-semibold text-3xl">
+              {info.level}
             </h1>
           </div>
         </div>
+        <button
+          onClick={handleLogout}
+          className="bg-white w-full text-main-blue border border-main-blue px-4 py-2 rounded-full shadow-sm transition-transform duration-500 ease-in-out hover:bg-main-blue hover:text-white"
+        >
+          Log out
+        </button>
+
       </div>
-      <div
-        className="flex flex-col items-center my-6
-      bg-white w-5/6 rounded-3xl
-      pt-4
-      shadow-lg
-      h-[60vh]"
-      >
+      {/* <div className="flex flex-col items-center my-6 bg-white w-5/6 rounded-3xl pt-4 shadow-lg h-[60vh]">
         <h1 className="text-[#4b5563]">Top Learners</h1>
-      </div>
+      </div> */}
     </div>
   );
 }
