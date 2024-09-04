7e40b30b3db94cf66dceaa3cf9d3337e0a307b0e
bobaonhan123 <hosybaonhan2004@gmail.com>
2024-09-05 06:10:59 +0700

    allow permission

--- src/Pages/AIPage.jsx
+++ src/Pages/AIPage.jsx
@@ -4,6 +4,7 @@ export default function AIPage() {
     return (
         <div className=" w-screen h-screen bg-purple mx-0 my-0">
             <iframe src={AIServerURL} frameborder="0"
+            allow="microphone; camera; geolocation; midi; encrypted-media"
             className="w-screen h-screen"></iframe>
         </div>
     )
