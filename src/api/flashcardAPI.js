30c847245ece19733568f337da349a91c01ac42e
trnghuybru <huytr222@gmail.com>
2024-09-05 22:00:24 +0700

    atch get flashcard api with ui and filter

--- src/api/flashcardAPI.js
+++ src/api/flashcardAPI.js
@@ -1,16 +1,48 @@
-import { server } from '../configurations/AxiosConfigurations';
+import { server } from "../configurations/AxiosConfigurations";
 export const createFlashcardSet = async (flashcardSet) => {
-    try {
-        const token = localStorage.getItem('token');
-        console.log(token)
-        const response = await server.post("/flashcard", flashcardSet, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error creating flashcard set:', error);
-        throw error;
-    }
+  try {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    const response = await server.post("/flashcard", flashcardSet, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error creating flashcard set:", error);
+    throw error;
+  }
+};
+
+export const getUserFlashcardSet = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await server.get("/flashcard", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error getting flashcard set:", error);
+    throw error;
+  }
+};
+
+export const getGlobalFlashcardSet = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await server.get("/flashcard/all", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error getting flashcard set:", error);
+    throw error;
+  }
 };
