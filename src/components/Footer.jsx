6993e11f804279bdd32b2563cb72877cd1d5bf0e
truong08112004 <113773627+truong08112004@users.noreply.github.com>
2024-09-05 08:14:56 +0700

    add:mainpanel,update:footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 import { footerHiddenRoutes } from "../configurations/HiddenComponentConfigurations";
 import { useLocation } from "react-router-dom";
 
@@ -13,9 +14,8 @@ function Footer() {
     <footer
       className="mx-0 my-0
         h-64 mt-10 flex flex-col justify-center
-        max-lg:h-auto"
-    >
-      <hr className="border-t-1 border-[#6c7580a8] mx-96 max-lg:mx-10" />
+        max-lg:h-auto">
+      <hr className="border-t-1 border-[#bdbac0] mx-96 max-lg:mx-10" />
       <div
         className="flex
             h-44
@@ -28,27 +28,58 @@ function Footer() {
             max-lg:h-auto
             max-lg:items-start
             max-lg:my-6
-            "
-      >
+            ">
         <div className="flex flex-col items-start max-lg:my-2">
-          <p className="font-semibold text-lg">About</p>
-          <p className="text-sm text-[#6C7580]">Pricing</p>
-          <p className="text-sm text-[#6C7580]">Contact</p>
+          <p className="mb-4 text-sm font-semibold uppercase text-[#4c5664]">
+            About
+          </p>
+          <div className="flex flex-col space-y-3">
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Pricing
+            </p>
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Contact
+            </p>
+          </div>
         </div>
         <div className="flex flex-col items-start max-lg:my-2">
-          <p className="font-semibold text-lg">Product</p>
-          <p className="text-sm text-[#6C7580]">Update</p>
-          <p className="text-sm text-[#6C7580]">Blog</p>
+          <p className="mb-4 text-sm font-semibold uppercaseg text-[#4c5664]">
+            Product
+          </p>
+          <div className="flex flex-col space-y-3">
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Update
+            </p>
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Blog
+            </p>
+          </div>
         </div>
         <div className="flex flex-col items-start max-lg:my-2">
-          <p className="font-semibold text-lg">Social</p>
-          <p className="text-sm text-[#6C7580]">Facebook</p>
-          <p className="text-sm text-[#6C7580]">Instagram</p>
+          <p className="mb-4 text-sm font-semibold uppercase text-[#4c5664]">
+            Social
+          </p>
+          <div className="flex flex-col space-y-3">
+              <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+                Facebook
+              </p>
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Instagram
+            </p>
+          </div>
         </div>
         <div className="flex flex-col items-start max-lg:my-2">
-          <p className="font-semibold text-lg">Help & Info</p>
-          <p className="text-sm text-[#6C7580]">Terms And Conditions</p>
-          <p className="text-sm text-[#6C7580]">Privacy Policy</p>
+          <p className="mb-4 text-sm font-semibold uppercase text-[#4c5664]">
+            Help & Info
+          </p>
+          <div className="flex flex-col space-y-3">
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline ">
+              Terms And Conditions
+            </p>
+            <p className="text-sm text-[#6C7580] text-hover flex w-fit items-center space-x-2 capitalize hover:underline">
+              Privacy Policy
+            </p>
+          </div>
         </div>
       </div>
       <p className="text-sm mb-2 text-[#4B5563]">Made by Khỉ Con</p>
